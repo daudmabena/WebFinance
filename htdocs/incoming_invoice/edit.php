c10f5d02502eb0d77672f38c46d3865dd85a0d80
Cyril Bouthors <cyril.bouthors@isvtec.com>
2014-03-18 13:11:47 +0100

    * htdocs/incoming_invoice/edit.php: add link to create a new provider

--- htdocs/incoming_invoice/edit.php
+++ htdocs/incoming_invoice/edit.php
@@ -128,6 +128,9 @@ while($row_provider = mysql_fetch_assoc($result_provider))
 ?>
 
    </select>
+   &nbsp;
+   <a href="/prospection/fiche_prospect.php?action=_new" onclick="return ask_confirmation('Are you sure you want to create a new provider?')" target="_blank">Add provider</a>
+
    </td>
   </tr>
 
