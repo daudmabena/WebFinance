ca5cb6da421f2311ce092a55d637b5b7fa0a3969
Cyril Bouthors <cyril.bouthors@isvtec.com>
2014-03-18 15:01:03 +0100

    * htdocs/incoming_invoice/upload.php: add support for ZIP upload

--- htdocs/incoming_invoice/upload.php
+++ htdocs/incoming_invoice/upload.php
@@ -19,7 +19,72 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+function upload_file($file = '', $filename = '')
+{
+  // Calculate MD5
+  $md5 = md5_file($file);
+
+  $destination_file = rtrim('../../incoming_invoice/' . chunk_split($md5, 4, '/'), '/');
+
+  // Create directory
+  system("mkdir -p " . dirname($destination_file), $rc);
+  if ($rc != 0)
+    die('mkdir failed');
+
+  // Check if destination file already exists
+  if(file_exists($destination_file))
+  {
+    $md5 = md5_file($destination_file);
+    echo "File <a href=\"edit.php?md5=$md5\">$filename</a> already exists, skipped<br/>\n";
+    return FALSE;
+  }
+
+  $note = mysql_real_escape_string($filename);
+  mysql_query("INSERT INTO incoming_invoice SET md5='$md5', note='$note'")
+    or die(mysql_error());
+
+  // Move the uploaded file to the final destination
+  if (!rename($file, $destination_file))
+    die("Failed renaming $file to $destination_file");
+
+  echo "File <a href=\"edit.php?md5=$md5\">$filename</a> uploaded<br/>\n";
+  return TRUE;
+}
+
+function upload_zip($zip_file = '')
+{
+  // Create temporary directory
+  exec('mktemp -d', $output, $rc);
+  if ($rc != 0)
+    die('mktemp failed');
+
+  $temp_dir = $output[0];
+  if(!is_dir($temp_dir))
+    die('Temporary directory not created');
+
+  // Unzip
+  system("unzip -q -d $temp_dir $zip_file", $rc);
+  if ($rc != 0)
+    die('unzip failed');
+
+  unlink($zip_file);
+
+  // Fetch directory listing
+  $file_list = glob("$temp_dir/*");
+
+  // Fetch file information
+  $files = array();
+  foreach($file_list as $file)
+  {
+    if(!preg_match('/\.pdf$/', $file))
+      continue;
+
+    upload_file($file, basename($file));
+  }
+}
+
 require("../inc/main.php");
+echo '<pre>';
 
 $User = new User();
 if(!$User->isAuthorized("manager,accounting,employee")){
@@ -29,7 +94,7 @@ if(!$User->isAuthorized("manager,accounting,employee")){
 }
 
 if(!isset($_FILES['file']['name']))
-  die('Too few argument');
+  die('Too few argument. PHP upload limit reached?');
 
 WebfinanceDocument::ValidateFileName($_FILES['file']['name']);
 CybPHP_Validate::ValidateInt($_SESSION['id_user']);
@@ -37,28 +102,24 @@ CybPHP_Validate::ValidateInt($_SESSION['id_user']);
 if($_FILES['file']['error'] !== 0)
   die('Unknown upload error from PHP');
 
-# Calculate MD5
-$md5 = md5_file($_FILES['file']['tmp_name']);
+$file_extension = preg_replace('/.*\./', '', $_FILES['file']['name']);
 
-$upload_file = rtrim('../../incoming_invoice/' . chunk_split($md5, 4, '/'), '/');
+switch($file_extension)
+{
+  case 'zip':
+    upload_zip($_FILES['file']['tmp_name']);
+    break;
 
-# Create directory
-system("mkdir -p " . dirname($upload_file), $rc);
-if ($rc != 0)
-  die('mkdir failed');
+  case 'pdf':
+  case 'PDF':
+    upload_file($_FILES['file']['tmp_name'], $_FILES['file']['name']);
+    break;
 
-// Check if destination file already exists
-if(file_exists($upload_file))
-  die("File $upload_file already exists");
+  default:
+    die("Unknown file extension $file_extension. Only PDF and ZIP are supported.");
+    break;
+}
 
-mysql_query("INSERT INTO incoming_invoice
-SET md5='$md5', note='".$_FILES['file']['name']."'")
-  or die(mysql_error());
-
-// Move the uploaded file to the final destination
-if (!move_uploaded_file($_FILES['file']['tmp_name'], $upload_file))
-  die("Failed uploading $upload_file");
-
-header('Location: ./edit.php?md5=' . $md5);
-exit;
 ?>
+
+<a href="./">done</a>
