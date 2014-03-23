2e066a03c0aa209011b8137e3c905729b3d25cc8
Cyril Bouthors <cyril.bouthors@isvtec.com>
2014-03-23 18:18:58 +0100

    * htdocs/inc/tab/fiche_prospect_log.php: better HTML layout

--- htdocs/inc/tab/fiche_prospect_log.php
+++ htdocs/inc/tab/fiche_prospect_log.php
@@ -21,7 +21,7 @@
 
 global $Client;
 ?>
-<table width="100%" border="0" cellspacing="0" cellpadding="5">
+<table border="0" cellspacing="0" cellpadding="5">
      <!-- Ugly, close the previous bloody global form. I mean *global* form! -->
      </form>
 <?php
