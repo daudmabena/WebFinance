f06e6c7bd39895eeb3c77dcfb0751a22bd57ab8e
Cyril Bouthors <cyril.bouthors@isvtec.com>
2014-03-24 11:53:12 +0100

    * htdocs/document/index.php: only show paid status for invoices

--- htdocs/document/index.php
+++ htdocs/document/index.php
@@ -269,7 +269,10 @@ while($row = mysql_fetch_assoc($result))
  </td>
 
  <td>
-   <img src="/imgs/icons/<?=$status_icon?>" title="Paid status: <?=$row[paid]?>">
+   <?
+     if($row['type']=='invoice')
+       echo "<img src=\"/imgs/icons/$status_icon\" title=\"Paid status: $row[paid]\">";
+   ?>
    <a href="/document/edit.php?md5=<?=$row[md5]?>&provider_id_filter=<?=$_GET[provider_id_filter]?>"><img src="/imgs/icons/edit.png" border="0" title="Edit invoice"></a>
  </td>
 
